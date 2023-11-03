b456d048e76dae774685ea8ff07faa5b1f1ee0e4
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-03 10:16:15 -0300

    Updated timestamp and added ID

--- internal/utils/db_data.go
+++ internal/utils/db_data.go
@@ -15,6 +15,7 @@ import (
 func GetExperienceData() []*models.Experience {
 	return []*models.Experience{
 		{
+			ID:       1,
 			JobTitle: "Software Development Intern",
 			Company:  "Encora Brazil Division",
 			Details: pq.StringArray{
@@ -22,10 +23,11 @@ func GetExperienceData() []*models.Experience {
 				"Responsible for helping the automation of the deployment of these distributed cloud platforms.",
 			},
 			CurrentJob: false,
-			StartDate:  time.Date(2022, time.January, 10, 0, 0, 0, 0, time.UTC),
-			EndDate:    time.Date(2022, time.August, 15, 0, 0, 0, 0, time.UTC),
+			StartDate:  time.Date(2022, time.January, 10, 12, 0, 0, 0, time.UTC),
+			EndDate:    time.Date(2022, time.August, 15, 12, 0, 0, 0, time.UTC),
 		},
 		{
+			ID:       2,
 			JobTitle: "Software Development Assistant",
 			Company:  "Encora Brazil Division",
 			Details: pq.StringArray{
@@ -33,11 +35,11 @@ func GetExperienceData() []*models.Experience {
 				"Collaborating with DevOps, QA and other teams to improve the software delivery process",
 				"Development and maintenance of Debian packages utilizing the dpkg-buildpackage tool",
 				"Responsible for writing, maintaining, and enhancing configuration files for Infrastructure as Code projects (IaaC).",
-				"Responsible for maintenance and development of improvements for automated tests, continuous integration pipeline and automation of deployments on bare metal servers using Jenkins.Responsible for maintenance and development of improvements for automated tests, continuous integration pipeline and automation of deployments on bare metal servers using Jenkins.",
+				"Responsible for maintenance and development of improvements for automated tests, continuous integration pipeline and automation of deployments on bare metal servers using Jenkins.",
 				"Teamwork using Agile methodology to deliver different product features in different sprints, working together with different teams from different nationalities and cultures.",
 			},
 			CurrentJob: true,
-			StartDate:  time.Date(2022, time.August, 15, 0, 0, 0, 0, time.UTC),
+			StartDate:  time.Date(2022, time.August, 15, 12, 0, 0, 0, time.UTC),
 		},
 	}
 }
@@ -46,6 +48,7 @@ func GetExperienceData() []*models.Experience {
 func GetProjectsData() []*models.Projects {
 	return []*models.Projects{
 		{
+			ID:    1,
 			Title: "Personal website",
 			Details: pq.StringArray{
 				"Development of a personal website containing my CV, posts and contact information",
@@ -54,7 +57,7 @@ func GetProjectsData() []*models.Projects {
 				"CI/CD Pipelines to automatically update both the website and the API",
 			},
 			LinkToRepo: "https://github.com/ldatb/website",
-			Timestamp:  time.Date(2023, time.October, 1, 0, 0, 0, 0, time.UTC),
+			Timestamp:  time.Date(2023, time.October, 1, 12, 0, 0, 0, time.UTC),
 		},
 	}
 }
@@ -63,46 +66,52 @@ func GetProjectsData() []*models.Projects {
 func GetCoursesData() []*models.Courses {
 	return []*models.Courses{
 		{
+			ID:              1,
 			Title:           "Containers & Kubernetes Essentials",
 			OfferedBy:       "IBM",
 			CertificateLink: "https://www.credly.com/badges/20571be2-15d5-4571-9ee0-725dec1087c2",
 			Duration:        time.Hour * 6,
-			Timestamp:       time.Date(2021, time.May, 31, 0, 0, 0, 0, time.UTC),
+			Timestamp:       time.Date(2021, time.May, 31, 12, 0, 0, 0, time.UTC),
 		},
 		{
+			ID:              2,
 			Title:           "Linux LPI Essentials",
 			OfferedBy:       "Alura",
 			CertificateLink: "https://cursos.alura.com.br/degree/certificate/5294ab09-9e1a-4bd1-aa45-752792af9a43",
 			Duration:        time.Hour * 30,
-			Timestamp:       time.Date(2022, time.October, 3, 0, 0, 0, 0, time.UTC),
+			Timestamp:       time.Date(2022, time.October, 3, 12, 0, 0, 0, time.UTC),
 		},
 		{
+			ID:              3,
 			Title:           "DevOps Formation",
 			OfferedBy:       "Alura",
 			CertificateLink: "https://cursos.alura.com.br/degree/certificate/8874a862-084a-49bf-8a59-33b4e1bb5ced",
 			Duration:        time.Hour * 58,
-			Timestamp:       time.Date(2022, time.November, 7, 0, 0, 0, 0, time.UTC),
+			Timestamp:       time.Date(2022, time.November, 7, 12, 0, 0, 0, time.UTC),
 		},
 		{
+			ID:              4,
 			Title:           "Software Engineering",
 			OfferedBy:       "Alura",
 			CertificateLink: "https://cursos.alura.com.br/degree/certificate/1482b9c6-5a51-49ba-8da9-ca765a9ddf59",
 			Duration:        time.Hour * 86,
-			Timestamp:       time.Date(2022, time.November, 8, 0, 0, 0, 0, time.UTC),
+			Timestamp:       time.Date(2022, time.November, 8, 12, 0, 0, 0, time.UTC),
 		},
 		{
+			ID:              5,
 			Title:           "Infra as a Code",
 			OfferedBy:       "Alura",
 			CertificateLink: "https://cursos.alura.com.br/degree/certificate/6e5989fa-2053-4613-a0da-ba683db2e640",
 			Duration:        time.Hour * 46,
-			Timestamp:       time.Date(2023, time.January, 26, 0, 0, 0, 0, time.UTC),
+			Timestamp:       time.Date(2023, time.January, 26, 12, 0, 0, 0, time.UTC),
 		},
 		{
+			ID:              6,
 			Title:           "Mathematics for Machine Learning",
 			OfferedBy:       "Imperial College London",
 			CertificateLink: "https://www.coursera.org/account/accomplishments/specialization/4A3RWFUTUFPW",
 			Duration:        time.Hour * 40,
-			Timestamp:       time.Date(2023, time.March, 27, 0, 0, 0, 0, time.UTC),
+			Timestamp:       time.Date(2023, time.March, 27, 12, 0, 0, 0, time.UTC),
 		},
 	}
 }
@@ -111,12 +120,13 @@ func GetCoursesData() []*models.Courses {
 func GetEducationData() []*models.Education {
 	return []*models.Education{
 		{
+			ID:              1,
 			DegreeType:      "Technology Degree",
 			DegreeName:      "Software Development",
 			CollegeName:     "UNIFBV Wyden",
 			CollegeLocation: "Brazil",
-			StartDate:       time.Date(2022, time.April, 14, 0, 0, 0, 0, time.UTC),
-			EndDate:         time.Date(2024, time.June, 1, 0, 0, 0, 0, time.UTC),
+			StartDate:       time.Date(2022, time.April, 14, 12, 0, 0, 0, time.UTC),
+			EndDate:         time.Date(2024, time.June, 1, 12, 0, 0, 0, time.UTC),
 		},
 	}
 }

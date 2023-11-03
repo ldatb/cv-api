cf1a4e06dfdf4a1e0b1b283e078c0f583d659630
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-03 10:16:23 -0300

    Use global variables

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,14 +8,13 @@ import (
 
 	"github.com/ldatb/cv-api/configs"
 	"github.com/ldatb/cv-api/internal/routes"
-	"gorm.io/gorm"
+	"github.com/ldatb/cv-api/internal/utils"
 
 	"github.com/gofiber/fiber/v2"
-	log "github.com/sirupsen/logrus"
 )
 
 // Initialize the API
-func Start(port int, logger *log.Logger, database *gorm.DB) {
+func Start(port int) {
 	// Get Fiber configs
 	config := configs.FiberConfig()
 
@@ -23,11 +22,11 @@ func Start(port int, logger *log.Logger, database *gorm.DB) {
 	app := fiber.New(config)
 
 	// Routes
-	routes.PublicRoutes(app, database)
+	routes.PublicRoutes(app)
 	routes.RouteNotFound(app)
 
 	// Start API
 	if err := app.Listen(fmt.Sprintf(":%d", port)); err != nil {
-		logger.Fatalf("Oops... Looks like the server failed to start! Reason: %v", err)
+		utils.Logger.Fatalf("Oops... Looks like the server failed to start! Reason: %v", err)
 	}
 }

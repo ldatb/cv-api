7af3578266ff2b2f37d974aae437e1b8ca1a8db6
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-11-02 23:32:35 -0300

    Moving from shell scripts to a Makefile

--- build/run.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023 Lucas de Ataides <ldatb@icloud.com>
-#
-# SPDX-License-Identifier: MIT
-#!/usr/bin/bash
-
-ROOT_DIR=$(dirname $(readlink -f "$0"))
-RUN_FILE="cv-api.go"
-
-go run ${ROOT_DIR}/../cmd/${RUN_FILE}
